e9f759efd1508d51bb9a36f9d2a63d52d4d9f6ec
Manoj G M <manojgm1812002@gmail.com>
2026-03-03 12:47:04 +0530

    updated

---
+++ src/loops/dowhile/Prog1.java
@@ -0,0 +1,20 @@
+package loops.dowhile;
+
+public class Prog1 {
+    public static void main(String[] args) {
+
+
+        int i = 10;
+        int j=5;
+
+        do {
+            System.out.println(i);
+            i--;
+        }while (i>=1);
+
+        do {
+            System.out.println(j);
+            i--;
+        }while (i>5);
+    }
+}
